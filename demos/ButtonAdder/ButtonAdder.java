1513c86de2e99bf816f384ea33be7dd1753d9fdf
Trevor Krenz <krenztr@rose-hulman.edu>
2012-10-03 23:57:50 -0400

    Added demos

---
+++ demos/ButtonAdder/ButtonAdder.java
@@ -0,0 +1,41 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class ButtonAdder implements IBrahmaPlugin
+{
+	private JFrame frame;
+	private JButton button;
+	
+	public ButtonAdder(JFrame jframe)
+	{
+		this.frame = jframe;
+		this.button = new JButton("Button");
+		this.button.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(frame,
+					    "The button has been clicked.");
+			}
+			
+		});
+	}
+	
+	@Override
+	public void start() {
+		frame.add(button);
+		
+	}
+
+	@Override
+	public void stop() {
+		frame.remove(button);
+		
+	}
+
+}
